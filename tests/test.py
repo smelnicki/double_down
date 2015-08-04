40c81597be3f8d88a52b73678ae890d7cc0e8572
smelnicki <smelnicki3@gmail.com>
2015-08-03 19:17:06 -0700

    fixing unit tests

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,24 @@
 import unittest
 
+from double_down import double_down
 
-class DoubleDownTestCase(unittest.TestCase):
-    pass
+
+class TestDoubleDownDecorator(unittest.TestCase):
+
+    def setUp(self):
+        self.arr = list(range(2))
+
+    def test_decorator(self):
+        @double_down
+        def append(arr, num):
+            arr.append(num)
+            return arr
+
+        result = append(self.arr, -1)
+        expected = [0, 1, -1, -1]
+
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
